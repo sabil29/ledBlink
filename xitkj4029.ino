45e0770a0174caea05099a8bc5ac8d086e14a4af
sabil29 <salsabilaaisy47@gmail.com>
2016-10-03 13:33:50 +0700

    Add files via upload

---
+++ xitkj4029.ino
@@ -0,0 +1,21 @@
+/* 
+  Blink
+  Menyalakan LED pada pin 6 selama 4 detik dan Mematikanya selama 4 detik.
+
+   Ini contoh kode Programnya.
+ */
+
+ int led = 6 ;//Output pada Digital Pin adalah sebuah LED yang terkoneksi/terhubung dengan Digital pin 6.
+  
+void setup() 
+{
+  pinMode(led, OUTPUT);// Mengatur Digital Pin sebagai Output.
+
+}
+
+void loop() {
+  digitalWrite(led, HIGH);          // Mengatur LED agar Menyala.
+  delay(3500);                      // Tunggu sampai 4 detik.
+  digitalWrite(led ,LOW);           // Mengatur LED agar Mati.
+  delay(3500);                      // Tunggu sampai 4 detik. 
+}
